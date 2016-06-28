5577117dc1b259d4220824ba85a65a7d02ccd4fe
Fabien Muselet <FM@imac27p5k.home>
2016-06-28 07:00:14 +0200

    bidouille

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,6 +13,26 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ReportingCtrl', function($scope) {
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBtnbxsLJdtnrWA1xqYTBBdQ1NXgKXPf7Q",
+    authDomain: "applikmvehic.firebaseapp.com",
+    databaseURL: "https://applikmvehic.firebaseio.com/",
+    storageBucket: "applikmvehic.appspot.com",
+  };
+  firebase.initializeApp(config);
+
+  var donnees;
+
+  firebase.database().ref('/users/').on('value', function(data){
+    donnees = data.val();
+    //utilisateurs = JSON.stringify(data.val());
+    console.log(donnees);
+  });
+
+  donnees = [ {"Nom": "Billy" , "Prenom": "Bob"} ,  {"Nom": "Marcel", "Prenom": "Paul"} ];
+
+  $scope.users = donnees;
 
 })
 
